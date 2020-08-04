42fa77329918ef1bd75ad1a71c0c6ab3caa39b83
internetcharles <charlesodellsmith@gmail.com>
2020-08-04 11:08:50 -0700

    structure

---
+++ src/ImageList/ImageList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ImageItem from './ImageItem'
+
+export default class ImageList extends Component {
+    render() {
+        return (
+            <div>
+                <ImageItem />
+            </div>
+        )
+    }
+}
