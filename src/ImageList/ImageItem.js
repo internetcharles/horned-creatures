e2449c6d6a5be5501283343503e06938f607ade7
internetcharles <charlesodellsmith@gmail.com>
2020-08-04 15:54:27 -0700

    searches don't clash anymore

--- src/ImageList/ImageItem.js
+++ src/ImageList/ImageItem.js
@@ -10,11 +10,20 @@ export default class ImageItem extends Component {
             <div>
                 {this.props.data
                 .filter(creature => {
-                    if(!this.props.keyword && !this.props.hornCount) return true;
+                    if(!this.props.keyword && !this.props.hornCount) { 
+                        return true;
+                    }
+                    else if (!this.props.keyword && this.props.hornCount)
+                    {
+                        return creature.horns.toString() === this.props.hornCount;
+                    }
+                    else if (this.props.keyword && !this.props.hornCount) {
+                        return this.props.keyword === creature.keyword;
+                    }
                     return creature.horns.toString() === this.props.hornCount && this.props.keyword === creature.keyword;
                 })
                 .map((image) =>
-                    <img src={image.url} key={image.title} alt={image.title} />
+                    <img src={image.url.includes('modernfarmer') ? 'http://www.placekitten.com/200/200' : image.url} key={image.title} alt={image.title} />
                     )}
             </div>
         )
